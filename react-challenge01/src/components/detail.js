3f7043dc357ba8867e09db5d80d59b52f01c808b
andreanwirapradana <andreanwirapradana@gmail.com>
2021-04-28 20:51:05 +0700

    challenge router

---
+++ react-challenge01/src/components/detail.js
@@ -0,0 +1,80 @@
+import React, {useEffect, useState} from 'react';
+import { useHistory, useParams } from "react-router-dom";
+
+function Home() {
+    let [pokemon, setPokemon] = useState({});
+    let history = useHistory();
+    const {id} = useParams();
+
+    useEffect(() => {
+        let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+        fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            setPokemon(data);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, [])
+
+    const goHome = () => {
+        history.push('/');
+    }
+
+    if (!pokemon.name) {
+        return <h1>Loading...</h1>
+    }
+
+    return (
+        <div>
+            <div>
+                <h1>{pokemon.name}</h1>
+                <img src={pokemon.sprites.front_default} alt="Pokemon_sprite" />
+            </div>
+
+            <div>
+                <h3>Types:</h3>
+                <ul>
+                    {
+                        pokemon.types.map((type, idx) => {
+                            return <li>{type.type.name}</li>
+                        })
+                    }
+                </ul>
+            </div>
+            <div>
+                <h3>Abilities:</h3>
+                <ul>
+                    {
+                        pokemon.abilities.map((ability, idx) => {
+                            return <li>{ability.ability.name}</li>
+                        })
+                    }
+                </ul>
+            </div>
+            <div>
+                <h3>Stats:</h3>
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Power</th>
+                    </tr>
+                    {
+                        pokemon.stats.map((stat, idx) => {
+                            return (
+                                <tr>
+                                    <td>{stat.stat.name}</td>
+                                    <td>{stat.base_stat}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                </table>
+            </div>
+            <button onClick={goHome}>Go Home</button>
+        </div>
+    )
+};
+
+export default Home;
---
+++ react-challenge01/src/components/home.js
@@ -0,0 +1,64 @@
+import React, {useState, useEffect} from 'react';
+import {Link, useHistory} from 'react-router-dom';
+
+function Detail() {
+    let [pokemons, setPokemons] = useState([]);
+    const history = useHistory();
+
+    useEffect(() => {
+        fetch('https://pokeapi.co/api/v2/pokemon/')
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data.results)
+            setPokemons(data.results);
+        })
+    }, [])
+
+    const goDetail = (url) => {
+        let output = "";
+        let flag = true;
+        for (let i = 34; i < url.length; i++) {
+            if (flag) {
+                output += url[i]
+            }
+
+            if (url[i+1] === '/') {
+                flag = false;
+            }
+
+        }
+        return `/pokemon/${output}`
+    }
+
+    if (pokemons.length === 0) {
+        return <h1>Loading...</h1>
+    }
+
+    return (
+        <div>
+            <h2>Pokemon Website</h2>
+            <button onClick={goDetail}>Go detail</button>
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>Action</th>
+                </tr>
+                {
+                    pokemons.map((pokemon, idx) => {
+                        return (
+                            <tr>
+                                <td>{pokemon.name}</td>
+                                <td>
+                                    <button><Link to={goDetail(pokemon.url)} >See Detail</Link></button>
+                                </td>
+                            </tr>
+                        )
+                    })
+                }
+            </table>
+        </div>
+    )
+};
+
+
+export default Detail;
